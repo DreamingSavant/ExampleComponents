aea2f6434c3e97ada1088a9027845f21b20c8f8c
Roderick P <rodpwood@icloud.com>
2026-03-03 14:41:29 -0500

    Initial Commit

---
+++ ExampleComponents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleComponents
+//
+//  Created by Rod on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleComponents/ExampleComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleComponentsApp.swift
+//  ExampleComponents
+//
+//  Created by Rod on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
